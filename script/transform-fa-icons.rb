01b75d8e74a7e93db2bd35a12a4ec744a84d76cd
anosim114 <arnold_siemens@pm.me>
2023-12-08 22:17:51 +0100

    setup rubocop locally

--- script/transform-fa-icons.rb
+++ script/transform-fa-icons.rb
@@ -5,55 +5,51 @@ require 'optparse'
 
 @options = {}
 OptionParser.new do |opt|
-  opt.on('--input INPUT'){ |o| @options[:input] = o }
-  opt.on('--output OUTPUT'){ |o| @options[:output] = o }
+  opt.on('--input INPUT') { |o| @options[:input] = o }
+  opt.on('--output OUTPUT') { |o| @options[:output] = o }
 end.parse!
 
-if @options[:input] == nil || @options[:output] == nil
+if @options[:input].nil? || @options[:output].nil?
   puts 'error: no input or output privided'
   exit
 end
 
 puts @options
-puts "Starting transformation..."
+puts 'Starting transformation...'
 
-def get_file_paths
-  files = Dir::children @options[:input]
-  files
+def file_paths
+  Dir.children @options[:input]
 end
 
-def open_file fn
-  fd = File.open "#{@options[:input]}/#{fn}"
-  fd
+def open_file(filename)
+  File.open "#{@options[:input]}/#{filename}"
 end
 
-def transform_file fd
-  xml = Nokogiri fd.read
+def transform_file(filedescriptor)
+  xml = Nokogiri filedescriptor.read
   svg = xml.at('svg')
-  zero, nother, width, height = svg['viewBox'].split ' '
-  svg["width"] = width
-  svg["height"] = height
-  tx = xml.to_xml
+  _zero, _nother, width, height = svg['viewBox'].split ' '
+  svg['width'] = width
+  svg['height'] = height
+  xml.to_xml
 end
 
-def write_file tx, fn
-  fn.gsub! '.svg', '-512.svg'
-  out_file_path = "#{@options[:output]}/#{fn}"
+def write_file(transformed_xml, filename)
+  filename.gsub! '.svg', '-512.svg'
+  out_file_path = "#{@options[:output]}/#{filename}"
   file = File.new out_file_path, 'w'
-  file.write tx
+  file.write transformed_xml
 end
 
 ############################################
 
-unless Dir.exist? @options[:output]
-  Dir::mkdir @options[:output]
-end
+Dir.mkdir @options[:output] unless Dir.exist? @options[:output]
 
-files = get_file_paths
+files = file_paths
 files.each do |fn| # filename
-  fd  = open_file fn # filename to filedescriptor
+  fd = open_file fn # filename to filedescriptor
   tf = transform_file fd # file descriptor to transformed xml
   write_file tf, fn # transformed xml to file with filename
 end
 
-puts "Done!"
+puts 'Done!'
